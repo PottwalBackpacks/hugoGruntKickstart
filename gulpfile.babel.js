90ab8586cb98f05e0d7d7f60e9ec764b8471e63b
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2018-01-24 13:33:44 +0100

    switching to victor-hugo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
 import gulp from "gulp";
-import {spawn} from "child_process";
+import {
+	spawn
+} from "child_process";
 import hugoBin from "hugo-bin";
 import gutil from "gulp-util";
 import postcss from "gulp-postcss";
@@ -10,6 +12,12 @@ import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
 import less from "gulp-less";
 import path from "path";
+import svgSprite from "gulp-svg-sprites";
+import responsive from "gulp-responsive";
+import spritesmith from "gulp.spritesmith";
+import folders from "gulp-folders";
+import gulpSequence from'gulp-sequence';
+
 
 const browserSync = BrowserSync.create();
 
@@ -22,8 +30,14 @@ gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, hugoArgsPreview));
 
 // Build/production tasks
-gulp.task("build", ["less", "js"], (cb) => buildSite(cb, [], "production"));
-gulp.task("build-preview", ["less", "js"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
+gulp.task("build", ["less", "js", "sprites","img-sequence", "copyVideo", "favicon"], (cb) => buildSite(cb, [], "production"));
+
+gulp.task('img-sequence', function (cb) {
+  gulpSequence("frame-animations","responsive_animations","images", cb)
+})
+
+
+gulp.task("build-preview", ["less", "js", "sprites","frame-animations", "images",  "copyVideo", "favicon"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
 
 // // Compile CSS with PostCSS
 // gulp.task("css", () => (
@@ -33,57 +47,195 @@ gulp.task("build-preview", ["less", "js"], (cb) => buildSite(cb, hugoArgsPreview
 //     .pipe(browserSync.stream())
 // ));
 
-gulp.task('less', function () {
-	 	gulp.src('./src/less/main.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./dist/css'))
+gulp.task('less', function() {
+	gulp.src('./src/less/main.less')
+		.pipe(less({
+			paths: [path.join(__dirname, 'less', 'includes')]
+		}))
+		.pipe(gulp.dest('./dist/css'))
 		.pipe(browserSync.stream());
+});
+
+gulp.task('copyVideo', function() {
+	gulp.src('./src/video/**.*')
+		.pipe(gulp.dest('./dist/assets/video'));
+});
+
+gulp.task('favicon', function() {
+	gulp.src('./src/favicons/**.*')
+		.pipe(gulp.dest('./dist/assets/favicons'));
 });
 
 // Compile Javascript
 gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
+	const myConfig = Object.assign({}, webpackConfig);
 
-  webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
+	webpack(myConfig, (err, stats) => {
+		if (err) throw new gutil.PluginError("webpack", err);
+		gutil.log("[webpack]", stats.toString({
+			colors: true,
+			progress: true
+		}));
+		browserSync.reload();
+		cb();
+	});
 });
+
+//task for svg sprites
+gulp.task('sprites', function() {
+	return gulp.src('./src/svg/*.svg')
+		.pipe(svgSprite({
+			mode: "symbols"
+		}))
+		.pipe(gulp.dest("./dist/assets/svg"));
+});
+
+//task for responsive images
+gulp.task('images', function() {
+	return gulp.src(['src/images/*.{jpg,png}'])
+		.pipe(responsive({
+			'*': [{
+					rename: {
+						suffix: '-original'
+					},
+				},{
+					width: 100,
+					withoutEnlargement: false,
+					rename: {
+						suffix: '-thumbnail'
+					},
+				}, {
+					width: 1024,
+					withoutEnlargement: false,
+					rename: {
+						suffix: '-large'
+					},
+				}, {
+					width: 800,
+					withoutEnlargement: false,
+					rename: {
+						suffix: '-medium'
+					},
+				}, {
+					width: 600,
+					withoutEnlargement: false,
+					rename: {
+						suffix: '-small'
+					},
+				}, {
+					width: 2048,
+					withoutEnlargement: false,
+					rename: {
+						suffix: '-retina'
+					},
+				}
+			]
+		}, {
+			// Global configuration for all images
+			// The output quality for JPEG, WebP and TIFF output formats
+			quality: 70,
+			// Use progressive (interlace) scan for JPEG and PNG output
+			progressive: true,
+			// Zlib compression level of PNG output format
+			compressionLevel: 6,
+			// Strip all metadata
+			withMetadata: false,
+
+			skipOnEnlargement: false,
+			errorOnEnlargement: false,
+
+
+
+		}))
+		.pipe(gulp.dest('dist/assets/img'));
+});
+
+
+//task for responsive images
+gulp.task('responsive_animations', function() {
+	return gulp.src(['src/frameAnimationsCompound/*.{jpg,png}'])
+		.pipe(responsive({
+			'*': [ {
+					width: 800,
+					withoutEnlargement: false,
+					rename: {
+						suffix: '-medium'
+					},
+				}, {
+					width: 600,
+					withoutEnlargement: false,
+					rename: {
+						suffix: '-small'
+					},
+				},
+			]
+		}, {
+			// Global configuration for all images
+			// The output quality for JPEG, WebP and TIFF output formats
+			quality: 70,
+			// Use progressive (interlace) scan for JPEG and PNG output
+			progressive: true,
+			// Zlib compression level of PNG output format
+			compressionLevel: 6,
+			// Strip all metadata
+			withMetadata: false,
+
+			skipOnEnlargement: false,
+			errorOnEnlargement: false,
+
+		}))
+		.pipe(gulp.dest('dist/assets/animations'));
+});
+
+//task for animations
+
+var frameAnimationsFolder = 'src/frameAnimation';
+
+gulp.task('frame-animations', folders(frameAnimationsFolder, function(folder) {
+	console.log("reading from ", folder)
+	var spriteData = gulp.src(path.join(frameAnimationsFolder, folder, '*.png')).pipe(spritesmith({
+		imgName: folder.concat(".png"),
+		cssName: folder.concat(".css"),
+		algorithm: 'top-down',
+		algorithmOpts: {
+			sort: false
+		}
+	}));
+	return spriteData.pipe(gulp.dest('src/frameAnimationsCompound'));
+}));
 
 // Development server with browsersync
 gulp.task("server", ["hugo", "less", "js"], () => {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  });
-  gulp.watch("./src/js/**/*.js", ["js"]);
+	browserSync.init({
+		server: {
+			baseDir: "./dist"
+		}
+	});
+	gulp.watch("./src/js/**/*.js", ["js"]);
 	gulp.watch("./src/less/**/*.less", ["less"]);
-  gulp.watch("./site/**/*", ["hugo"]);
+	gulp.watch("./site/**/*", ["hugo"]);
+	gulp.watch("./src/svg/**/*", ["sprites"]);
+	gulp.watch("./src/images/**/*", ["images"]);
+
 });
 
 /**
  * Run hugo and build the site
  */
 function buildSite(cb, options, environment = "development") {
-  const args = options ? hugoArgsDefault.concat(options) : hugoArgsDefault;
+	const args = options ? hugoArgsDefault.concat(options) : hugoArgsDefault;
 
-  process.env.NODE_ENV = environment;
+	process.env.NODE_ENV = environment;
 
-  return spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
-    if (code === 0) {
-      browserSync.reload();
-      cb();
-    } else {
-      browserSync.notify("Hugo build failed :(");
-      cb("Hugo build failed");
-    }
-  });
+	return spawn(hugoBin, args, {
+		stdio: "inherit"
+	}).on("close", (code) => {
+		if (code === 0) {
+			browserSync.reload();
+			cb();
+		} else {
+			browserSync.notify("Hugo build failed :(");
+			cb("Hugo build failed");
+		}
+	});
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,155 @@
-// JS Goes here - ES6 supported
+import jQuery from 'jquery';
+const $ = jQuery;
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+
+
+let ImagesLoaded = require('imagesloaded');
+let jQueryBridget = require('jquery-bridget');
+let Masonry = require('masonry-layout');
+
+jQueryBridget( 'masonry', Masonry, $ );
+jQueryBridget( 'imagesLoaded', ImagesLoaded, $ );
+
+
+
+import './scrolloverflow';
+
+import fullpage from 'fullpage.js/dist/jquery.fullpage.extensions.min';
+
+import rb_image from "./responsive_background_images"
+
+var loadedSlide;
+
+$(document).ready(function() {
+	$('#svgContainer').load('/assets/svg/svg/symbols.svg');
+
+	//start loading the responsive_background_images
+let elements = document.querySelectorAll('.responsive-background-image');
+for (let i = 0; i < elements.length; i++) {
+	new rb_image(elements[i]);
+}
+
+	$('#fullpage').fullpage({
+		parallax: false,
+		parallaxKey: 'cG90dHdhbC5pdF83OHZjR0Z5WVd4c1lYZz0wOHQ=',
+		scrollOverflow: true,
+		lazyLoading: true,
+
+
+		//events
+		onLeave: function(index, nextIndex, direction) {
+			var currentSlide = $(this);
+
+
+			var nextSlide;
+			if (direction == 'up') {
+				nextSlide = $(currentSlide).prev();
+			} else {
+				nextSlide = $(currentSlide).next();
+			}
+
+			var activeSlide = nextSlide.find('.fp-slide.active');
+			if (activeSlide.length !== 0 && activeSlide.data('ui-color')) {
+				nextSlide = activeSlide;
+			}
+
+
+
+			lazyLoad(nextSlide);
+
+
+			if (index == 1) {
+				$('#navigation').toggleClass('small', true);
+				$('.navbar-brand-small').toggleClass('center', true);
+
+			}
+			if (nextIndex == 1) {
+				$('#navigation').toggleClass('small', false);
+				$('.navbar-brand-small').toggleClass('center', false);
+
+
+			}
+		},
+		afterLoad: function(anchorLink, index) {
+			loadedSlide = $(this);
+			checkIfInverted();
+		},
+	});
+
+
+
+	//  init Masonry
+	let $grid = $('.grid');
+	$grid.imagesLoaded(function () {
+	    $grid.masonry({
+	        itemSelector: '.grid-item',
+					columnWidth: '.grid-sizer',
+					gutter: '.gutter-sizer',
+					itemSelector: '.grid-item',
+					percentPosition: false
+
+	    });
+	 });
+
+
+	 // // init Masonry
+	 // var $grid = $('.grid').masonry({
+	 // 	itemSelector: '.grid-item',
+	 // 	// use element for option
+	 // 	columnWidth: '.grid-sizer',
+	 // 	percentPosition: true
+	 // });
+		//
+	 // // layout Masonry after each image loads
+	 // $grid.imagesLoaded().progress(function() {
+	 // 	$grid.masonry('layout');
+	 // });
+
+
+});
+
+
+
+
+//handle netlify redirect after auth
+if (window.netlifyIdentity) {
+	window.netlifyIdentity.on("init", user => {
+		if (!user) {
+			window.netlifyIdentity.on("login", () => {
+				document.location.href = "/admin/";
+			});
+		}
+	});
+}
+
+function checkIfInverted() {
+	if ($(loadedSlide).hasClass('inverted')) {
+		$('body').toggleClass('inverted', true);
+	} else {
+		$('body').toggleClass('inverted', false);
+	}
+}
+
+
+function lazyLoad(section) {
+	//console.log(section);
+	var element;
+
+	section.find('img[data-src], img[data-srcset], source[data-src], video[data-src], audio[data-src], iframe[data-src]').each(function() {
+		element = $(this);
+
+		$.each(['src', 'srcset'], function(index, type) {
+			var attribute = element.attr('data-' + type);
+			if (typeof attribute !== 'undefined' && attribute) {
+				setSrc(element, type);
+			}
+		});
+	});
+}
+
+
+function setSrc(element, attribute) {
+	element
+		.attr(attribute, element.data(attribute))
+		.removeAttr('data-' + attribute);
+}
